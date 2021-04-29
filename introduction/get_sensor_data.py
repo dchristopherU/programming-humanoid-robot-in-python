30be7cc37f6d3e405085286c20e0a905a252a6d3
d.christopher <christopher.dukart@outlook.de>
2021-04-29 18:27:56 +0200

    ha1

--- introduction/get_sensor_data.py
+++ introduction/get_sensor_data.py
@@ -2,8 +2,10 @@
 In this exercise you need to know how to get sensor data.
 
 * Task: get the current joint angle and temperature of joint HeadYaw
+  Ermitteln Sie den aktuellen Gelenkwinkel und die Temperatur des Gelenkkopfes
 
 * Hint: The current sensor data of robot are store in perception (class Perception in spark_agent.py)
+  Die aktuellen Sensordaten des Roboters werden in der Wahrnehmung gespeichert (Klasse Perception in spark_agent.py).
 
 '''
 
@@ -17,12 +19,13 @@ from spark_agent import SparkAgent
 
 class MyAgent(SparkAgent):
     def think(self, perception):
-        angle = 0
-        temperature = 0
+        angle = perception.joint['HeadYaw']
+        temperature = perception.joint_temperature['HeadYaw']
         # YOUR CODE HERE
         # get angle and temperature to current data of joint HeadYaw
 
-        print 'HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature)
+
+        print ('HeadYaw angle: ' + str(angle) + ' temperature: ' + str(temperature))
         return super(MyAgent, self).think(perception)
 
 if '__main__' == __name__:
